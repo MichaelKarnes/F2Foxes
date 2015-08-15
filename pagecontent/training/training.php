07de6805905eba2333bb4eb0704137a665683878
dtmcderm <dtmcderm@tamu.edu>
2015-08-15 00:19:51 -0500

    minor modification. fixed height issue.

--- pagecontent/training/training.php
+++ pagecontent/training/training.php
@@ -2,7 +2,7 @@
 <div id="main-wrapper">
 	<div class="wrapper style3">
         <div id="training_page title">
-           <iframe src="https://docs.google.com/spreadsheets/d/1wJodUEryd_OzVQJ-5_HO8yhVE1PAIy1AqidGQqzdtZ0/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" width="100%" height="600px"></iframe>
+           <iframe src="https://docs.google.com/spreadsheets/d/1wJodUEryd_OzVQJ-5_HO8yhVE1PAIy1AqidGQqzdtZ0/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" width="100%" height="500px" style="border-width:0"></iframe>
 	    </div>			
 	</div>
 </div>
