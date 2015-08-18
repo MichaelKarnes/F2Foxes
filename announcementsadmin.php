acd74f189e2a94ee24d16c7d491c9e35744b5224
PMyers1732 <go_military_usa.pm111@yahoo.com>
2015-08-17 19:19:09 -0500

    added tab

--- announcementsadmin.php
+++ announcementsadmin.php
@@ -1,14 +1,45 @@
 <?php
-
+include 'boot/session.php'; 
+if (!($_SESSION['student']==1)){ die("Sorry you do not have access to this file "); }  
 ?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Announcements</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="no-sidebar">
+		<div id="page-wrapper">
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        
-    </body>
+            <!--header-->
+            <?php include 'pagecontent/header.php'; ?>
+
+            <div id="main-wrapper">
+	            <div class="wrapper style3">
+		            <div class="inner">
+			            <div class="container">
+				            <div class="content">
+                                <!--Main Content-- link to about.php it is located in the pagecontent folder-->
+                                 <?php include 'pagecontent/announcementsadmin/announcementsadmin.php'?>
+				            </div>
+			            </div>
+		            </div>
+	            </div>
+            </div>
+            
+			<!-- Footer  -->
+            <?php include 'pagecontent/footer.php'; ?>
+				
+
+		</div>
+
+		<!--Script-- can be found in the pagecontent folder script.php-->
+        <?php include 'pagecontent/script.php'?>
+
+	</body>
 </html>
