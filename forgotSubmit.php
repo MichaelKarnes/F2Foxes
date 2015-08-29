8a5617e4a11912e5e0212bcd124f065bb27ee28e
dtmcderm <dtmcderm@tamu.edu>
2015-08-28 23:20:00 -0500

    working on forgot

--- forgotSubmit.php
+++ forgotSubmit.php
@@ -36,15 +36,19 @@ include 'boot/session.php';
                 if($newPass != $newPass2) {
                     echo '<p>Passwords do not match.<a href ="forgot.php"> Please Try Again.</a></p>';
                 } 
-                elseif(strlen($newPass) > 25 || strlen($newPass) < 6) {
-                    echo '<p>Passwords must be between 6 and 25 characters.<a href ="forgot.php"> Please Try Again.</a></p>';
+                elseif(strlen($newPass) >= 25 || strlen($newPass) <= 6) {
+                    echo '<p>Passwords must be between 6 to 25 characters.<a href ="forgot.php"> Please Try Again.</a></p>';
                 }
                 #insert new password into the database for admin to see the next time admin 
                 #logs on. See login.php     
                 else {
                     $userForgot = $_POST['userForgot'];
-                    #insert date time (the null ' '), the user who Forgot, and the new desired password
-                    $query = $db->query("INSERT INTO Forgot_Password VALUES('', '$userForgot', '$newPass')");
+                    $timestamp = time();
+                    echo $timestamp;
+                    $date = date("M-D-Y");
+                    echo $date;
+                    #insert date and time , the user who Forgot, and the new desired password
+                    $query = $db->query("INSERT INTO Forgot_Password VALUES(' ', '$date', '$timestamp', '$userForgot', '$newPass')");
                     echo '<p>Your request has been succesfully sent to the website admins,
                     David M and Peter M. One of us will contact you by phone for confirmation 
                     within the next 24 hours. Once we verify your request, your password will 
