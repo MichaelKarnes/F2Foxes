dcb90be45fd0151c0ad9ee5374d37a3713be139e
PMyers1732 <go_military_usa.pm111@yahoo.com>
2015-08-30 17:20:05 -0500

    made it good for posting

--- boot/session.php
+++ boot/session.php
@@ -2,8 +2,8 @@
 session_start();
 
 //comment out next line if working from home 
- //$db = mysqli_connect("localhost", "km310765_admin", "Aftermath2015", "km310765_f2foxes") or die($connect_error);
+ $db = mysqli_connect("localhost", "km310765_admin", "Aftermath2015", "km310765_f2foxes") or die($connect_error);
 
  // comment out next line if working from pushed website
- $db=mysqli_connect("192.232.249.164", "km310765_admin", "Aftermath2015","km310765_f2foxes") or die ("Couldn't connect!");
+ //$db=mysqli_connect("192.232.249.164", "km310765_admin", "Aftermath2015","km310765_f2foxes") or die ("Couldn't connect!");
 ?>
