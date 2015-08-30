55abce8588dd647fdd45971790313e1e3de6e8b7
MichaelKarnes <mkarnes2015@gmail.com>
2015-08-30 14:28:51 -0500

    Example of inside & outside interaction.

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-   #this is a test
-   echo("Hello World!"); 
+    include_once 'core/init.php';
+    $user = new User();
+    /*$salt = utf8_decode(utf8_encode(Hash::salt(32)));
+    echo $salt;
+    $user->create(array(
+        'username' => 'admin',
+        'password' => Hash::make('15A6B!z217', $salt),
+        'salt' => utf8_encode($salt),
+        'name' => 'Mr. Fox',
+        'group' => 1
+    ));
+    die();*/
+    if($user->isLoggedIn()) {
+        require_once 'home/inside.php';
+    } else {
+        require_once 'home/outside.php';
+    }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        
-    </body>
-</html>
